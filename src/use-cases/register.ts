83927537f53b15f5a5527814c9c79b6e73bd1b4d
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-01-14 23:31:56 -0300

    :test_tube: test: Add user register tests

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import type { UsersRepositoryInterface } from '@/repositories/users-repository.js'
 import bcrypt from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
+import type { User } from 'generated/prisma/client.js'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,8 +9,12 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export class RegisterUseCase {
-  // Inversão de dependência, a clase RegisterUseCase agora está acoplada à interface UsersRepositoryInterface, então ela pode receber como parâmetro qualquer outra classe que implemente essa mesma interface.
+interface RegisterUseCaseResponse {
+  execute: (data: RegisterUseCaseRequest) => Promise<{ user: User }>
+}
+
+export class RegisterUseCase implements RegisterUseCaseResponse {
+  // Inversão de dependência, a classe RegisterUseCase agora está acoplada à interface UsersRepositoryInterface, então ela pode receber como parâmetro qualquer outra classe que implemente essa mesma interface.
   constructor(private usersRepository: UsersRepositoryInterface) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
